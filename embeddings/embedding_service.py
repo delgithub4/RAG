b69c688cf49bd756e90b7589008d79e8b4f0f216
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-27 22:56:45 +0100

    Create embedding_service.py

---
+++ embeddings/embedding_service.py
@@ -0,0 +1,17 @@
+from sentence_transformers import SentenceTransformer
+
+
+class EmbeddingService:
+
+    def __init__(self):
+
+        self.model = SentenceTransformer(
+            "all-MiniLM-L6-v2"
+        )
+
+    def create(
+        self,
+        text
+    ):
+
+        return self.model.encode(text)
