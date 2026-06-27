a618b886ef416f647af3ee0cf53e9a8e80e6cd53
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-27 22:37:19 +0100

    Create text_loader.py

---
+++ loaders/text_loader.py
@@ -0,0 +1,11 @@
+class TextLoader:
+
+    def load(self, path):
+
+        with open(
+            path,
+            "r",
+            encoding="utf-8"
+        ) as file:
+
+            return file.read()
